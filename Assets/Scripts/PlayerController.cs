bb658707717d4304c3fbb0745d5ca15fc3b60bd8
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2019-04-16 17:03:26 -0400

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text timerText;
+    
+
+
+    private Rigidbody rb;
+    private int count;
+    private GameObject[] pickUps;
+    private float targetTime = 5.0f;
+
+
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        pickUps = GameObject.FindGameObjectsWithTag("Pick Up");
+        PickUpsUpdate();
+    }
+
+    void FixedUpdate()
+    {   
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            PickUpsUpdate();
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString() + "/10";
+        if (count >= 10)
+        {
+            winText.text = "You win";
+        }
+    }
+
+    void PickUpsUpdate()
+    {
+        foreach (var item in pickUps)
+        {
+            item.SetActive(false);
+        }
+        pickUps[count].SetActive(true);
+    }
+
+    
+
+    void Update()
+    {
+        targetTime -= Time.deltaTime;
+        SetTimerText();
+        if (targetTime <= 20.0f)
+        {
+            timerText.color = Color.yellow;
+            if (targetTime <= 10.0f)
+            {
+                timerText.color = Color.red;
+            }
+            if (targetTime <= 0.0f)
+            {
+                timerEnded();
+            }
+        }
+        
+    }
+
+    void SetTimerText()
+    {
+        timerText.text = "Remaining time: " + targetTime.ToString("0.0");
+    }
+
+    void timerEnded()
+    {
+        winText.text = "You lost!";
+        SetCountText();
+        Time.timeScale = 0;
+        targetTime = 0.0f;
+        SetTimerText();
+    }
+}
